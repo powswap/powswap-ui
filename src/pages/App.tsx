5020753da209309bd95b1ce34dcd9bd79ff1d8c4
kornerglass <112548574+kornerglass@users.noreply.github.com>
2022-09-02 03:21:57 +0900

    Disable unneeded paths

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -5,11 +5,10 @@ import Loader from 'components/Loader'
 import TopLevelModals from 'components/TopLevelModals'
 import { useFeatureFlagsIsLoaded } from 'featureFlags'
 import { NavBarVariant, useNavBarFlag } from 'featureFlags/flags/navBar'
-import { NftVariant, useNftFlag } from 'featureFlags/flags/nft'
 import { TokensVariant, useTokensFlag } from 'featureFlags/flags/tokens'
 import ApeModeQueryParamReader from 'hooks/useApeModeQueryParamReader'
 import { lazy, Suspense, useEffect } from 'react'
-import { Navigate, Route, Routes, useLocation } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import { useIsDarkMode } from 'state/user/hooks'
 import styled from 'styled-components/macro'
 import { SpinnerSVG } from 'theme/components'
@@ -24,30 +23,19 @@ import Navbar from '../components/NavBar'
 import Popups from '../components/Popups'
 import { useIsExpertMode } from '../state/user/hooks'
 import DarkModeQueryParamReader from '../theme/DarkModeQueryParamReader'
-import AddLiquidity from './AddLiquidity'
-import { RedirectDuplicateTokenIds } from './AddLiquidity/redirects'
 import { RedirectDuplicateTokenIdsV2 } from './AddLiquidityV2/redirects'
 import Earn from './Earn'
 import Manage from './Earn/Manage'
-import MigrateV2 from './MigrateV2'
-import MigrateV2Pair from './MigrateV2/MigrateV2Pair'
-import Pool from './Pool'
-import { PositionPage } from './Pool/PositionPage'
 import PoolV2 from './Pool/v2'
 import PoolFinder from './PoolFinder'
 import RemoveLiquidity from './RemoveLiquidity'
-import RemoveLiquidityV3 from './RemoveLiquidity/V3'
 import Swap from './Swap'
-import { OpenClaimAddressModalAndRedirectToSwap, RedirectPathToSwapOnly, RedirectToSwap } from './Swap/redirects'
+import { RedirectPathToSwapOnly, RedirectToSwap } from './Swap/redirects'
 import { LoadingTokenDetails } from './TokenDetails'
 import Tokens, { LoadingTokens } from './Tokens'
 
 const TokenDetails = lazy(() => import('./TokenDetails'))
 const Vote = lazy(() => import('./Vote'))
-const NftExplore = lazy(() => import('nft/pages/explore'))
-const Collection = lazy(() => import('nft/pages/collection'))
-const Sell = lazy(() => import('nft/pages/sell/sell'))
-const Asset = lazy(() => import('nft/pages/asset/Asset'))
 
 const AppWrapper = styled.div`
   display: flex;
@@ -113,7 +101,6 @@ export default function App() {
   const isLoaded = useFeatureFlagsIsLoaded()
   const tokensFlag = useTokensFlag()
   const navBarFlag = useNavBarFlag()
-  const nftFlag = useNftFlag()
 
   const { pathname } = useLocation()
   const currentPage = getCurrentPageFromLocation(pathname)
@@ -180,16 +167,16 @@ export default function App() {
                       />
                     </>
                   )}
-                  <Route
+                  {/* <Route
                     path="vote/*"
                     element={
                       <Suspense fallback={<LazyLoadSpinner />}>
                         <Vote />
                       </Suspense>
                     }
-                  />
-                  <Route path="create-proposal" element={<Navigate to="/vote/create-proposal" replace />} />
-                  <Route path="claim" element={<OpenClaimAddressModalAndRedirectToSwap />} />
+                  /> */}
+                  {/* <Route path="create-proposal" element={<Navigate to="/vote/create-proposal" replace />} /> */}
+                  {/* <Route path="claim" element={<OpenClaimAddressModalAndRedirectToSwap />} /> */}
                   <Route path="uni" element={<Earn />} />
                   <Route path="uni/:currencyIdA/:currencyIdB" element={<Manage />} />
 
@@ -199,43 +186,43 @@ export default function App() {
 
                   <Route path="pool/v2/find" element={<PoolFinder />} />
                   <Route path="pool/v2" element={<PoolV2 />} />
-                  <Route path="pool" element={<Pool />} />
-                  <Route path="pool/:tokenId" element={<PositionPage />} />
+                  {/* <Route path="pool" element={<Pool />} /> */}
+                  {/* <Route path="pool/:tokenId" element={<PositionPage />} /> */}
 
                   <Route path="add/v2" element={<RedirectDuplicateTokenIdsV2 />}>
                     <Route path=":currencyIdA" />
                     <Route path=":currencyIdA/:currencyIdB" />
                   </Route>
-                  <Route path="add" element={<RedirectDuplicateTokenIds />}>
-                    {/* this is workaround since react-router-dom v6 doesn't support optional parameters any more */}
+                  {/* <Route path="add" element={<RedirectDuplicateTokenIds />}>
+                    {/* this is workaround since react-router-dom v6 doesn't support optional parameters any more }
                     <Route path=":currencyIdA" />
                     <Route path=":currencyIdA/:currencyIdB" />
                     <Route path=":currencyIdA/:currencyIdB/:feeAmount" />
-                  </Route>
+                  </Route> */}
 
-                  <Route path="increase" element={<AddLiquidity />}>
+                  {/* <Route path="increase" element={<AddLiquidity />}>
                     <Route path=":currencyIdA" />
                     <Route path=":currencyIdA/:currencyIdB" />
                     <Route path=":currencyIdA/:currencyIdB/:feeAmount" />
                     <Route path=":currencyIdA/:currencyIdB/:feeAmount/:tokenId" />
-                  </Route>
+                  </Route> */}
 
                   <Route path="remove/v2/:currencyIdA/:currencyIdB" element={<RemoveLiquidity />} />
-                  <Route path="remove/:tokenId" element={<RemoveLiquidityV3 />} />
+                  {/* <Route path="remove/:tokenId" element={<RemoveLiquidityV3 />} /> */}
 
-                  <Route path="migrate/v2" element={<MigrateV2 />} />
-                  <Route path="migrate/v2/:address" element={<MigrateV2Pair />} />
+                  {/* <Route path="migrate/v2" element={<MigrateV2 />} />
+                  <Route path="migrate/v2/:address" element={<MigrateV2Pair />} /> */}
 
                   <Route path="*" element={<RedirectPathToSwapOnly />} />
 
-                  {nftFlag === NftVariant.Enabled && (
+                  {/* {nftFlag === NftVariant.Enabled && (
                     <>
                       <Route path="/nfts/collection/:contractAddress" element={<Collection />} />
                       <Route path="/nfts" element={<NftExplore />} />
                       <Route path="/nft/sell" element={<Sell />} />
                       <Route path="/nft/asset/:contractAddress/:tokenId" element={<Asset />} />
                     </>
-                  )}
+                  )} */}
                 </Routes>
               ) : (
                 <Loader />
