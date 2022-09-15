d64006b408dc140d778a2797403f50d24a3cf8ca
kornerglass <112548574+kornerglass@users.noreply.github.com>
2022-09-15 19:47:53 +0200

    add alchemy

--- src/constants/networks.ts
+++ src/constants/networks.ts
@@ -7,13 +7,15 @@ if (typeof INFURA_KEY === 'undefined') {
   throw new Error(`REACT_APP_INFURA_KEY must be a defined environment variable`)
 }
 
-export const MAINNET_PROVIDER = new JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_KEY}`)
+export const MAINNET_PROVIDER = new JsonRpcProvider(
+  `https://eth-mainnet.g.alchemy.com/v2/2wngR8Qeij_bqaoy4VOPgdVhOfNvIDvE`
+)
 
 /**
  * These are the network URLs used by the interface when there is not another available source of chain data
  */
 export const RPC_URLS: { [key in SupportedChainId]: string } = {
-  [SupportedChainId.MAINNET]: `https://mainnet.infura.io/v3/${INFURA_KEY}`,
+  [SupportedChainId.MAINNET]: `https://eth-mainnet.g.alchemy.com/v2/2wngR8Qeij_bqaoy4VOPgdVhOfNvIDvE`,
   [SupportedChainId.ETHW]: `https://mainnet.ethereumpow.org`,
   [SupportedChainId.GOERLI]: `https://goerli.infura.io/v3/${INFURA_KEY}`,
 }
