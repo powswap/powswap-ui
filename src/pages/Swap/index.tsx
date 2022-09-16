a2d1ab8c9f0d04a3c920627b8589879d48b92140
kornerglass <112548574+kornerglass@users.noreply.github.com>
2022-09-16 11:44:38 +0200

    remove signature approval for swap

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -315,25 +315,14 @@ export default function Swap() {
   } = useERC20PermitFromTrade(trade, allowedSlippage, transactionDeadline)
 
   const handleApprove = useCallback(async () => {
-    if (signatureState === UseERC20PermitState.NOT_SIGNED && gatherPermitSignature) {
-      try {
-        await gatherPermitSignature()
-      } catch (error) {
-        // try to approve if gatherPermitSignature failed for any reason other than the user rejecting it
-        if (error?.code !== 4001) {
-          await approveCallback()
-        }
-      }
-    } else {
-      await approveCallback()
+    await approveCallback()
 
-      sendEvent({
-        category: 'Swap',
-        action: 'Approve',
-        label: [TRADE_STRING, trade?.inputAmount?.currency.symbol].join('/'),
-      })
-    }
-  }, [signatureState, gatherPermitSignature, approveCallback, trade?.inputAmount?.currency.symbol])
+    sendEvent({
+      category: 'Swap',
+      action: 'Approve',
+      label: [TRADE_STRING, trade?.inputAmount?.currency.symbol].join('/'),
+    })
+  }, [approveCallback, trade?.inputAmount?.currency.symbol])
 
   // check if user has gone through approval process, used to show two step buttons, reset on token change
   const [approvalSubmitted, setApprovalSubmitted] = useState<boolean>(false)
