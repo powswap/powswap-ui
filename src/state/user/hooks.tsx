de6311674523cc4e3b12cb6a671e61a45f6972fb
kornerglass <112548574+kornerglass@users.noreply.github.com>
2022-09-19 16:25:16 +0200

    force dark mode

--- src/state/user/hooks.tsx
+++ src/state/user/hooks.tsx
@@ -57,7 +57,8 @@ export function useIsDarkMode(): boolean {
     shallowEqual
   )
 
-  return userDarkMode === null ? matchesDarkMode : userDarkMode
+  //return userDarkMode === null ? matchesDarkMode : userDarkMode
+  return true
 }
 
 export function useDarkModeManager(): [boolean, () => void] {
