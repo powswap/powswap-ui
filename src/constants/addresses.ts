6b48ea7d1bba3c3920fd2f8792275794c7263428
kornerglass <112548574+kornerglass@users.noreply.github.com>
2022-09-20 16:47:05 +0200

    update router address

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -1,5 +1,3 @@
-import { FACTORY_ADDRESS as V2_FACTORY_ADDRESS } from '@uniswap/v2-sdk'
-
 import { constructSameAddressMap } from '../utils/constructSameAddressMap'
 import { SupportedChainId } from './chains'
 
@@ -7,8 +5,8 @@ type AddressMap = { [chainId: number]: string }
 
 export const UNI_ADDRESS: AddressMap = constructSameAddressMap('0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984')
 
-export const V2_FACTORY_ADDRESSES: AddressMap = constructSameAddressMap(V2_FACTORY_ADDRESS)
-export const V2_ROUTER_ADDRESS: AddressMap = constructSameAddressMap('0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D')
+export const V2_FACTORY_ADDRESSES: AddressMap = constructSameAddressMap('0x2164Fc926cfc0112bd0aE3e55C2FC94522025E08')
+export const V2_ROUTER_ADDRESS: AddressMap = constructSameAddressMap('0x2F43FE90dA8E09afabDb2DF2398B53B7eC5952A8')
 
 /* V3 Contract Addresses */
 export const V3_CORE_FACTORY_ADDRESSES: AddressMap = {}
