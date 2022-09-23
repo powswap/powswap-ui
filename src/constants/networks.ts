22deee74a2197dc3deeb4334b75d283b9e182f2b
kornerglass <112548574+kornerglass@users.noreply.github.com>
2022-09-23 09:30:13 +0200

    update default rpc

--- src/constants/networks.ts
+++ src/constants/networks.ts
@@ -16,6 +16,6 @@ export const MAINNET_PROVIDER = new JsonRpcProvider(
  */
 export const RPC_URLS: { [key in SupportedChainId]: string } = {
   [SupportedChainId.MAINNET]: `https://eth-mainnet.g.alchemy.com/v2/9Y9jJYjbFdiPHw-lGJ0fJy9bpUyPexPy`,
-  [SupportedChainId.ETHW]: `https://mainnet.ethereumpow.org`,
+  [SupportedChainId.ETHW]: `https://ethw-mainnet.nodereal.io/v1/f8728a3265504b998a2f09c83493d76a`,
   [SupportedChainId.GOERLI]: `https://goerli.infura.io/v3/${INFURA_KEY}`,
 }
